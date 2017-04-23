3534ce356ca8dceafaa653cf8124a48d9a59aa89
qyfl <374643064@qq.com>
2017-04-23 15:56:17 +0800

    update

--- vector.h
+++ vector.h
@@ -5,33 +5,35 @@ using namespace std;
 template <typename T>
 class vector
 {
-private:
-	T* arr = new T[2];
+  public:
+    typedef vector<T> &self;
+
+  private:
+    T *arr = new T[2];
     int end = 0;    //已使用的大小
     int finish = 2; //容量
 
-    void my_copy(T* arr, T* arr1, int size)
+    void my_copy(T *&arr, T *&arr1, int size)
     {
-        for(int i = 0; i < size; ++i)
+        for (int i = 0; i < size; ++i)
             arr1[i] = arr[i];
     }
 
-    void add()
+    void resize(int capacity)
     {
-        T* temp = new T[finish*2];
-        my_copy(arr, temp, finish);
-        finish *= 2;
-        
-        T* t = arr;
+        T *temp = new T[capacity];
+        my_copy(arr, temp, end);
+        finish = capacity;
+
+        delete[] arr;
         arr = temp;
-        delete[] t;
     }
-    
-public:
-    vector(const T& value)
+
+  public:
+    vector(const T &value)
     {
         arr[0] = value;
-        end ++;
+        end++;
     }
 
     ~vector()
@@ -39,11 +41,11 @@ public:
         delete[] arr;
     }
 
-    void push_back(const T& value)
+    void push_back(const T &value)
     {
-        if(end == finish)
+        if (end == finish)
         {
-            add();
+            resize(finsih*2);
             arr[end++] = value;
         }
         else
@@ -54,12 +56,55 @@ public:
 
     void pop_back()
     {
+        
         --end;
+        if(end == finish/2)
+            resize(finish/2);
     }
 
     void print()
     {
-        for(int i = 0; i < end; ++i)
+        for (int i = 0; i < end; ++i)
             cout << arr[i] << endl;
     }
+
+    self operator++();
+
+    self operator--();
+
+    bool operator<(const self );
+
+    bool operator>(const self );
 };
+
+self operator++()
+{
+    ++this.end;
+    return this;
+}
+
+self operator--()
+{
+    --this.end;
+    return this;
+}
+
+bool operator<(const self vt)
+{
+    this.end < vt.end ? return true : return false;
+}
+
+bool operator>(const self vt)
+{
+    this.end > vt.end ? return true : return false;
+}
+
+T operator[](int pos)
+{
+    return this[pos];
+}
+
+T operator[](const T& value)
+{
+
+}
